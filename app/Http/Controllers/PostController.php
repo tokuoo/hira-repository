177cbac71928eb30859a8ee73471c21b21ad8dbf
tokuoo <eu20076-8948@sti.chubu.ac.jp>
2023-03-19 14:06:25 +0900

    ブログ投稿削除処理の実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -32,13 +32,19 @@ class PostController extends Controller
     
     public function edit(Post $post)
     {
-        return view( 'posts/edit' )->with(['post' => $post]);
+        return view('posts/edit')->with(['post' => $post]);
     }
     
     public function update(PostRequest $request, Post $post)
     {
         $input_post = $request['post'];
         $post->fill($input_post)->save();
-        return redirect('/posts/'. $post->id);
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect('/');
     }
 }
